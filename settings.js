1e4b30ee2d7898aa514e31b409713da1e6811331
PrinceOfDev <guardian02262@gmail.com>
2022-08-12 21:33:10 +0900

    ultimate

---
+++ settings.js
@@ -0,0 +1,9 @@
+const clientID = ''; // your clientID
+const clientSecret = ''; // your client Secret
+const redirect_uri = encodeURIComponent('http://localhost:3000/redirect'); // your domain
+
+export {
+    clientID,
+    clientSecret,
+    redirect_uri
+};
